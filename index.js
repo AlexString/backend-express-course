a8166498a4f0e1c2bafc7c088efa25cb46786b89
AlexString <ricardo.lopez2962@alumnos.udg.mx>
2022-03-31 10:06:44 -0600

    fix: cors permission origin

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 const whitelist = ['http://localhost:8080', 'https://myapp.com'];
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin)) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed'));
