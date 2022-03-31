b72ab9b5cc08556e1ff8259b36af61b54ae90e04
AlexString <ricardo.lopez2962@alumnos.udg.mx>
2022-03-30 20:55:48 -0600

    fix: api categories routing

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,11 @@ const categoriesRouter = require('./categories.router');
 
 function routerApi(app) {
   const router = express.Router();
+
   app.use('/api/v1', router);
   router.use('/products', productsRouter);
-  router.use('/api/categories', categoriesRouter);
-  router.use('/api/user', userRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/user', userRouter);
 }
 
 module.exports = routerApi;
