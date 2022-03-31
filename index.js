c53826416ded7edff94729e95723fca393333ee4
AlexString <ricardo.lopez2962@alumnos.udg.mx>
2022-03-31 08:35:11 -0600

    feat: middlewares implemented on products

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const routerApi = require('./routes');
 
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/error.handler');
+
 const app = express();
 const port = 3000;
 
@@ -11,6 +17,10 @@ app.get('/', (req, res) => {
 });
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Port running in ${port}`);
--- routes/products.router.js
+++ routes/products.router.js
@@ -14,10 +14,14 @@ router.get('/filter', (req, res) => {
   res.send('I am a filter');
 });
 
-router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const product = await service.findOne(id);
-  res.json(product);
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await service.findOne(id);
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post('/', async (req, res) => {
@@ -26,16 +30,14 @@ router.post('/', async (req, res) => {
   res.status(201).json(newProduct);
 });
 
-router.patch('/:id', async (req, res) => {
+router.patch('/:id', async (req, res, next) => {
   try {
     const { id } = req.params;
     const body = req.body;
     const product = await service.update(id, body);
     res.json(product);
   } catch (error) {
-    res.status(404).json({
-      message: error.message,
-    });
+    next(error);
   }
 });
 
